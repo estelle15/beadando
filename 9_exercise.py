0c54030c299efcfba656aacaab68c25f2437a257
estelle15 <36422006+estelle15@users.noreply.github.com>
2018-05-03 16:27:28 +0200

    Update 9_exercise.py

--- 9_exercise.py
+++ 9_exercise.py
@@ -1,1 +1,16 @@
+def max_prime(number):
+  factor=2:
+    while factor*factor<=number:
+      while number%factor==0:
+        number/=factor
+      factor+=1
+    if (number>1):
+      return number
+   return factor
 
+
+print max_prime(5)
+print max_prime(7)
+print max_prime(13)
+print max_prime(29)
+print max_prime(600851475143)
