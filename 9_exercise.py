2fa87d7beca79e158a5e913532dd77666d50d1aa
estelle15 <36422006+estelle15@users.noreply.github.com>
2018-05-03 16:35:48 +0200

    Update 9_exercise.py

--- 9_exercise.py
+++ 9_exercise.py
@@ -1,13 +1,12 @@
 def max_prime(number):
-	factor=2:
-	while factor*factor<=number:
-		while number%factor==0:
-    	number/=factor
-    factor+=1
-	if (number>1):
-    return number
-  return factor
-
+    factor=2
+    while factor * factor <= number:
+        while number%factor == 0:
+            number/=factor
+        factor+=1
+    if (number > 1):
+        return number
+    return factor
 
 print max_prime(5)
 print max_prime(7)
