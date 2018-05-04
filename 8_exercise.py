dbae107baa4f84879f02a5a8cbb6d9b4702899ef
estelle15 <36422006+estelle15@users.noreply.github.com>
2018-05-04 20:55:16 +0200

    Update 8_exercise.py

--- 8_exercise.py
+++ 8_exercise.py
@@ -1,1 +1,13 @@
+def longest_common(elso, masodik, x, y):
 
+	if x == 0 or y == 0:
+	    return 0;
+	elif elso[x-1] == masodik[y-1]:
+	    return 1 + longest_common(elso, masodik, x-1, y-1);
+	else:
+	    return max(longest_common(elso, masodik, x, y-1), longest_common(elso, masodik, x-1, y));
+
+
+elso = "FGHJKAERT"
+masodik= "HJKEORPD"
+print "Length of LCS is ", longest_common(elso , masodik, len(elso), len(masodik))
