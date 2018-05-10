7e3fdaa03593b2721270dffdcbe7ee3c5ea5a645
Kiss Eszter <36422006+estelle15@users.noreply.github.com>
2018-05-10 21:27:19 +0200

    Update 2_exercise.py

--- 2_exercise.py
+++ 2_exercise.py
@@ -1,1 +1,16 @@
-
+def convert(lista,b1,b2):
+    szam=0
+    helyiertek=1
+    for i in range(len(lista)-1,-1,-1):
+        if lista[i]>=b1:
+            print("Nem jó szám")
+            return None
+        szam+=lista[i]*helyiertek
+        helyiertek*=b1
+    lista2=[]
+    while szam!=0:
+        lista2.append(szam%b2)
+        szam//=b2
+    lista2.reverse()
+    return lista2
+print(convert([6,7,4],10,2))
